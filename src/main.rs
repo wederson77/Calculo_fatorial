5a67b2e3ce98b3e2f4929b4161ed194d0bdcc82d
wederson77 <wendersonpina10@gmail.com>
2024-04-29 09:35:25 -0300

    onlycommit

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,19 @@
 use std::io;
 
-fn convert_to_int(data_input: &String) -> i32{
+fn convert_to_int(data_input: & String) -> i32{
     let x = data_input.trim().parse::<i32>().unwrap();
-    x 
+    x
 }
 
-fn main() {
-    let mut number1 = String::new();
-    io::stdin().read_line(&mut number1).expect("Erro ao ler number1");
-    let mut number2 = String::new();
-    io::stdin().read_line(&mut number2).expect("Erro ao ler number1");
+fn main(){
+    let mut entrada_fatorial = String::new();
+    io::stdin().read_line(&mut entrada_fatorial).expect("Erro ao ler entrada fatorial");
+    let mut fatorial = 1;
+    let mut entrada_int = convert_to_int(&entrada_fatorial);
 
-    if convert_to_int(&number1) > convert_to_int(&number2){
-        println!("O numero {} é maior que o numero {}", number1, number2);
-    }else if convert_to_int(&number1) == convert_to_int(&number2){
-        println!("O numero {} é exatamente igual ao numero {}", number1, number2);
+    while entrada_int > 1{
+        fatorial = fatorial * entrada_int;
+        entrada_int = entrada_int -1;
     }
-    else{
-        println!("O numero {} é menor o numero {}", number1, number2);
-    }
+    println!("O fatorial é: {}", fatorial);
 }
